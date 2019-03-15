67a533801c719c63f70f570fa2e3b2e8fd5b719d
Jackson Forner <jef170030@utdallas.edu>
2019-03-15 01:20:35 -0500

    notes

---
+++ Chapter 1.m
@@ -0,0 +1,10 @@
+% 1.1 - Getting into Matlab
+
+% When the Matlab software is started, a window opens in which the main
+% part is the Command Window. The " >> " is called the "prompt".
+
+% The Command Window can be used interactively and Matlab will respond
+% immediately.
+
+% Useful starting functions: "demo", "help", "lookfor", "doc".
+% To exit Matlab, type either "quit" or "exit" at the prompt.
